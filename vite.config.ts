7687394c6a085f1ec515a80fe4d79ccc23551ef4
rodolfom9 <rodolfo_alto99@hotmail.com>
2025-05-20 00:25:01 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/rmcgeosite/' : '/',
+  base: mode === 'production' ? './' : '/',
   plugins: [
     react(),
     mode === 'development' &&
